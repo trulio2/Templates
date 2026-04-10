94bf00adf8b0f95d088701d12b317c1ec607751a
Trulio <tulio-santos@hotmail.com>
2026-04-09 21:18:06 -0300

    fix jwt env

--- NestJs/src/modules/auth/strategies/jwt.strategy.ts
+++ NestJs/src/modules/auth/strategies/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private configService: ConfigService
   ) {
     super({
-      secretOrKey: configService.get('JWT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
     })
   }
