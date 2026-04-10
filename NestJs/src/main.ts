048f8842248523d0919d69863c4f0ed7891fc171
Trulio <tulio-santos@hotmail.com>
2026-04-10 18:19:04 -0300

    fixing port error

--- NestJs/src/main.ts
+++ NestJs/src/main.ts
@@ -1,14 +1,13 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { RequestTimingInterceptor } from './interceptors'
 import { JsonLogger } from './observability/json-logger'
 import { startObservability } from './observability/observability'
-import { RequestTimingInterceptor } from './interceptors'
 
 async function bootstrap() {
   const port = Number(process.env.PORT ?? 3000)
   const logger = new JsonLogger()
-  let isShuttingDown = false
   let observability: { shutdown: () => Promise<void> } | undefined
 
   startObservability()
@@ -22,6 +21,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true
   })
+
   app.useLogger(logger)
   app.useGlobalInterceptors(new RequestTimingInterceptor())
   app.useGlobalPipes(
@@ -30,20 +30,7 @@ async function bootstrap() {
     })
   )
   app.enableCors()
-  app.enableShutdownHooks()
   await app.listen(port)
-
-  const shutdown = async () => {
-    if (isShuttingDown) return
-    isShuttingDown = true
-
-    await app.close()
-    await observability?.shutdown()
-  }
-
-  process.once('SIGINT', shutdown)
-  process.once('SIGTERM', shutdown)
-  process.once('SIGUSR2', shutdown)
 
   logger.verbose(`Application is running on port ${port}`)
 }
